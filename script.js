29fa1818803dab7620b85049eb3b00c4142628a7
Yusuf Kolawole <Yusufkolawoleolasile@gmail.com>
2019-12-09 17:54:59 +0100

    commit files

---
+++ script.js
@@ -0,0 +1,6 @@
+let root = document.documentElement;
+// let styles = getComputedStyle(root)
+// let mainColor = styles.getPropertyValue('--bg')
+// console.log(String(mainColor).trim())
+
+root.style.setProperty('--bg', 'teal')
