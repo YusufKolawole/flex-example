eba64542529e81f49863adad1c2b055b642cc9d6
Yusuf Kolawole <Yusufkolawoleolasile@gmail.com>
2019-12-09 18:16:03 +0100

    adding alert to script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let root = document.documentElement;
-// let styles = getComputedStyle(root)
-// let mainColor = styles.getPropertyValue('--bg')
-// console.log(String(mainColor).trim())
+// let root = document.documentElement;
+// // let styles = getComputedStyle(root)
+// // let mainColor = styles.getPropertyValue('--bg')
+// // console.log(String(mainColor).trim())
 
-root.style.setProperty('--bg', 'teal')
+// root.style.setProperty('--bg', 'teal')
+
+
+alert("hello world")
